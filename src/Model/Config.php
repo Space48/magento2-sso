9a1c82a87b6c08cd1c779e8a259ff365fca8361e
maximilian.grosch <max@team-mb.com>
2022-06-17 09:54:25 +0200

    Adde possibility to configure SAML attribute names

--- src/Model/Config.php
+++ src/Model/Config.php
@@ -9,9 +9,14 @@ class Config
 {
     private const XML_PATH_ENABLED = 'admin/space48_sso/enabled';
     private const XML_PATH_SP_ENTITY_ID = 'admin/space48_sso/sp/entity_id';
+    private const XML_PATH_SP_STATIC_MAGENTO_ROLE_NAME = 'admin/space48_sso/sp/static_magento_role_name';
     private const XML_PATH_IDP_ENTITY_ID = 'admin/space48_sso/idp/entity_id';
     private const XML_PATH_IDP_SIGN_ON_URL = 'admin/space48_sso/idp/sign_on_url';
     private const XML_PATH_IDP_PUBLIC_CERTIFICATE = 'admin/space48_sso/idp/public_certificate';
+    private const XML_PATH_IDP_ATTRIBUTE_FIRSTNAME = 'admin/space48_sso/idp/attribute_firstname';
+    private const XML_PATH_IDP_ATTRIBUTE_LASTNAME = 'admin/space48_sso/idp/attribute_lastname';
+    private const XML_PATH_IDP_ATTRIBUTE_EMAIL = 'admin/space48_sso/idp/attribute_email';
+    private const XML_PATH_IDP_ATTRIBUTE_ROLE = 'admin/space48_sso/idp/attribute_role';
 
     /**
      * @var Url
@@ -34,6 +39,36 @@ class Config
     public function isEnabled(): bool
     {
         return $this->config->isSetFlag(self::XML_PATH_ENABLED);
+    }
+
+    public function getFirstNameAttributeName(): string
+    {
+        return $this->config->getValue(self::XML_PATH_IDP_ATTRIBUTE_FIRSTNAME) ?? 'firstname';
+    }
+
+    public function getLastNameAttributeName(): string
+    {
+        return $this->config->getValue(self::XML_PATH_IDP_ATTRIBUTE_LASTNAME) ?? 'lastname';
+    }
+
+    public function getEmailAttributeName(): string
+    {
+        return $this->config->getValue(self::XML_PATH_IDP_ATTRIBUTE_EMAIL) ?? 'email';
+    }
+
+    public function geRoleAttributeName(): string
+    {
+        return $this->config->getValue(self::XML_PATH_IDP_ATTRIBUTE_ROLE) ?? 'role';
+    }
+
+    public function hasStaticMagentoRoleName(): bool
+    {
+        return $this->config->getValue(self::XML_PATH_SP_STATIC_MAGENTO_ROLE_NAME) !== null;
+    }
+
+    public function getStaticMagentoRoleName(): string
+    {
+        return $this->config->getValue(self::XML_PATH_SP_STATIC_MAGENTO_ROLE_NAME);
     }
 
     public function getSAMLSettings(): array
